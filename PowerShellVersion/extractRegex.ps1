4107da69ef862e81e8ba9527e36df553f93c0796
Evan <evan@evangriffith.net>
2015-06-05 17:18:19 -0700

    Initialized file and included PowerSHellVersion of Regex Stripper

---
+++ PowerShellVersion/extractRegex.ps1
@@ -0,0 +1,4 @@
+﻿$input_path = ‘C:\Users\evangrif\Desktop\Classics and Classics with a Twist.txt’
+$output_file = ‘C:\Users\evangrif\Desktop\Classics and Classics with a Twist Extracted.txt’
+$regex = ‘http://www(1)?.[a-z]*.[^>]*’
+select-string -Path $input_path -Pattern $regex -AllMatches | % { $_.Matches } | % { $_.Value } > $output_file
