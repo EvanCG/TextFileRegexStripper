f156256f183b8a4acb575d37c4aab0f54c0d92cc
Evan <evan@evangriffith.net>
2015-06-08 17:11:35 -0700

    Quick inconsequential change to Regex with Powershell

--- PowerShellVersion/extractRegex.ps1
+++ PowerShellVersion/extractRegex.ps1
@@ -1,4 +1,4 @@
-﻿$input_path = ‘C:\Users\evangrif\Desktop\Classics and Classics with a Twist.txt’
-$output_file = ‘C:\Users\evangrif\Desktop\Classics and Classics with a Twist Extracted.txt’
+﻿$input_path = ‘C:\Users\evangrif\Documents\OneNote Notebooks\0300 Gold\Classics and Classics with a Twist.one’
+$output_file = ‘C:\Users\evangrif\Desktop\Test Gold Extracted.txt’
 $regex = ‘http://www(1)?.[a-z]*.[^>]*’
 select-string -Path $input_path -Pattern $regex -AllMatches | % { $_.Matches } | % { $_.Value } > $output_file
